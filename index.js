7b41389c4f5e4f710aa343b0723efb6d9a8b78c3
Megan Horsfield <meganthadev@gmail.com>
2020-08-05 06:05:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 const createFareMultiplier = function(numOfTickets) {
   return function(fare) {
-    
+    return numOfTickets
   }
 }
