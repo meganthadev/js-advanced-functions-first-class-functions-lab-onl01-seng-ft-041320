84b46e044ec3ec6f39542eb6d8ce34be0a6f3b00
Megan Horsfield <meganthadev@gmail.com>
2020-08-05 06:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
 function selectDifferentDrivers(drivers, selection) {
-  return 
+  if selection === returnFirstTwoDrivers
 }
